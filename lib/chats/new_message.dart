5d43fed0d21a562ebad07408e6e4bdb541a063f5
MuhammadUsman186 <https://github.com/MuhammadUsman186>
2022-11-22 23:46:22 +0500

    initial commit

---
+++ lib/chats/new_message.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class NewMessages extends StatefulWidget {
+  const NewMessages({super.key});
+
+  @override
+  State<NewMessages> createState() => _NewMessagesState();
+}
+
+class _NewMessagesState extends State<NewMessages> {
+  final _controller = new TextEditingController();
+  var _enteredMessage = '';
+
+  void _sendMessage() async {
+    FocusScope.of(context).unfocus();
+    final user = await FirebaseAuth.instance.currentUser;
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user!.uid,
+    });
+    _controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.only(top: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'Send a Message...'),
+              onChanged: (value) {
+                setState(() {
+                  _enteredMessage = value;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
+            icon: Icon(Icons.send),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/auth_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterChat',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.purple,
+          backgroundColor: Colors.pink,
+        ),
+        buttonTheme: ButtonTheme.of(context).copyWith(
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, userSnapshot) {
+            if (userSnapshot.hasData) {
+              return ChatScreen();
+            }
+            return AuthScreen();
+          }),
+    );
+  }
+}
